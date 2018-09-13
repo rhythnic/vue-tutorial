7d58ab195db8dcdb9c609c9305ca056b9442e8a6
Nick Baroni <nick@rhythnic.com>
2018-09-13 16:59:47 -0400

    Added Gallery component.

---
+++ src/components/Gallery.vue
@@ -0,0 +1,37 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import pixabayApi from '../services/pixabay'
+
+export default {
+  name: 'gallery',
+
+  data () {
+    return {
+      images: []
+    }
+  },
+
+  methods: {
+    fetchImages () {
+      pixabayApi.getImages()
+        .then(x => x.hits)
+        .then(x => { this.images = x })
+        .catch(console.error)
+    }
+  },
+
+  created () {
+    this.fetchImages()
+  },
+
+  watch: {
+    images (val, oldVal) {
+      console.log('images array length', val.length)
+      console.log('previous images array length', oldVal.length)
+    }
+  }
+}
+</script>
