bae9ff7f86b2dfb06005c1741bae416079c4f532
Nick Baroni <nick@rhythnic.com>
2018-09-13 17:47:25 -0400

    Added image grid and computed blur.

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,11 +1,26 @@
 <template>
-  <div></div>
+  <div class="grid">
+    <GalleryImage
+      v-for="image in images"
+      :key="image.id"
+      class="image"
+      :src="image.previewURL"
+      :alt="image.tags"
+      :likes="image.likes"
+      :mostLikes="mostLikes"
+    />
+  </div>
 </template>
 
 <script>
 import pixabayApi from '../services/pixabay'
+import GalleryImage from './GalleryImage'
 
 export default {
+  components: {
+    GalleryImage
+  },
+
   name: 'gallery',
 
   data () {
@@ -14,24 +29,36 @@ export default {
     }
   },
 
+  computed: {
+    imagesSortedByLikes () {
+      return this.images.slice(0).sort((a, b) => b.likes < a.likes)
+    },
+    mostLikes () {
+      if (!this.images.length) return 0
+      return this.imagesSortedByLikes[this.images.length - 1].likes
+    }
+  },
+
   methods: {
     fetchImages () {
       pixabayApi.getImages()
-        .then(x => x.hits)
-        .then(x => { this.images = x })
+        .then(x => { this.images = x.hits })
         .catch(console.error)
     }
   },
 
   created () {
     this.fetchImages()
-  },
-
-  watch: {
-    images (val, oldVal) {
-      console.log('images array length', val.length)
-      console.log('previous images array length', oldVal.length)
-    }
   }
 }
 </script>
+
+<style scoped>
+  .grid {
+    display: flex;
+    flex-flow: row wrap;
+  }
+  .image {
+    margin: 16px;
+  }
+</style>
